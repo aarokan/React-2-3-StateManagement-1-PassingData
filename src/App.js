026a5becbb5fe9b3f03fe404aa9967dc456d25a6
aarokan <aarokan@gmail.com>
2020-01-06 09:47:37 +0000

    First Solution

--- src/App.js
+++ src/App.js
@@ -106,6 +106,12 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<ul>
+          {profiles.map((profile) => (
+            <li>{users[profile.userID].name}'s favorite movie is {movies[profile.favoriteMovieID].name}</li>
+          	)
+          )}
+		</ul>
       </div>
     );
   }
