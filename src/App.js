2e712219c7f100e34737b0f2b296d2e933afbe20
aarokan <aarokan@gmail.com>
2020-01-07 07:13:25 +0000

    Add a unique key prop to the list

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ class App extends Component {
         <h2>Favorite Movies</h2>
 		<ul>
           {profiles.map((profile) => (
-            <li>{users[profile.userID].name}'s favorite movie is {movies[profile.favoriteMovieID].name}</li>
+            <li key={profile.userID}>{users[profile.userID].name}'s favorite movie is {movies[profile.favoriteMovieID].name}</li>
           	)
           )}
 		</ul>
